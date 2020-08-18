eaf4afa24b738979bdd749032d0c9fc918cee555
Arnold Sandoval <arnold.sandoval@okta.com>
2020-08-18 16:22:43 -0700

    wip

--- packages/docs/docs/.vuepress/config.js
+++ packages/docs/docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: `arnie-ui Design System`,
+  title: `Odyssey Design System`,
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
