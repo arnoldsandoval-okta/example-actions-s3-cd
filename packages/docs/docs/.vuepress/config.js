f2892cf8c2feaa28a44cebb66fa4ca58693da161
Arnold Sandoval <arnold.sandoval@okta.com>
2020-08-28 20:32:44 -0700

    fix typo in workflow file

--- packages/docs/docs/.vuepress/config.js
+++ packages/docs/docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: `Odyssey Design System`,
+  title: `Odyssey`,
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
