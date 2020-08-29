a639d1c044034209910ac720a09a7565928e8300
Arnold Sandoval <arnold.sandoval@okta.com>
2020-08-28 20:30:02 -0700

    update title, deploy workflow to reflect new subdomain URL

--- packages/docs/docs/.vuepress/config.js
+++ packages/docs/docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: `Arnie Design System`,
+  title: `Odyssey Design System`,
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
