0cee4ba9994df27a2ca62180b7f7f8d4467b57df
Arnold Sandoval <arnold.sandoval@okta.com>
2021-01-11 21:44:03 -0800

    wip

--- scripts/notify-slack.sh
+++ scripts/notify-slack.sh
@@ -3,52 +3,52 @@
 LERNA_LIST=$(npx lerna list)
 PREVIEW_URLS=""
 for value in $LERNA_LIST; do
-  PREVIEW_URLS+="<https://$SHA7-$value|View$value>"
+  PREVIEW_URLS+="https://$SHA7-$value"
 done
 
 echo "${PREVIEW_URLS}" 
 
-# curl \
-#   -X POST \
-#   -H "Content-Type: application/json" \
-#   --data '
-#   {
-#     "blocks": [
-#       {
-#         "type": "context",
-#         "elements": [
-#           {
-#             "type": "mrkdwn",
-#             "text": ":rocket: *Preview Deployed*"
-#           },
-#           {
-#             "type": "mrkdwn",
-#             "text": "'"$AUTHOR_NAME"'@'"$SHA7"'"
-#           },
-#           {
-#             "type": "mrkdwn",
-#             "text": "'"$BRANCH_NAME"'"
-#           }
-#         ]
-#       },
-#       {
-#         "type": "section",
-#         "text": {
-#           "type": "mrkdwn",
-#           "text": "> '"$COMMIT_MSG"'"
-#         }
-#       },
-#       {
-#         "type": "section",
-#         "text": {
-#           "type": "mrkdwn",
-#           "text": "<https://github.com/okta/odyssey/pull/'"$PULL_REQUEST_ID"'|PR #'"$PULL_REQUEST_ID"'>'"$PREVIEW_URLS"'"
-#         }
-#       },
-#       {
-#         "type": "divider"
-#       }
-#     ]
-#   }
-#   ' \
-#   $INCOMING_WEBHOOK_URL
+curl \
+  -X POST \
+  -H "Content-Type: application/json" \
+  --data '
+  {
+    "blocks": [
+      {
+        "type": "context",
+        "elements": [
+          {
+            "type": "mrkdwn",
+            "text": ":rocket: *Preview Deployed*"
+          },
+          {
+            "type": "mrkdwn",
+            "text": "'"$AUTHOR_NAME"'@'"$SHA7"'"
+          },
+          {
+            "type": "mrkdwn",
+            "text": "'"$BRANCH_NAME"'"
+          }
+        ]
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": "> '"$COMMIT_MSG"'"
+        }
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": "<https://github.com/okta/odyssey/pull/'"$PULL_REQUEST_ID"'|PR #'"$PULL_REQUEST_ID"'>'"$PREVIEW_URLS"'"
+        }
+      },
+      {
+        "type": "divider"
+      }
+    ]
+  }
+  ' \
+  $INCOMING_WEBHOOK_URL
